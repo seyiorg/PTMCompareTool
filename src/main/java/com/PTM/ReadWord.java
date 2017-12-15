09925bb70309114417b3b9b6af629bd464b7e941
Olu <seyiorg@evidensconsultants.com>
2017-12-15 16:18:26 +0000

    wip

--- src/main/java/com/PTM/ReadWord.java
+++ src/main/java/com/PTM/ReadWord.java
@@ -1,4 +1,10 @@
 package com.PTM;
 
+import java.io.File;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+
 public class ReadWord {
+
+    File file = null;
+    WordExtractor extractor = null ;
 }
