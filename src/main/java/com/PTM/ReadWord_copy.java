2bb56fbc43b1dc438c86a09f8b2c84d0006a7b27
olu <seyiorg@evidensconsultants.com>
2017-12-21 09:27:44 +0000

    wip

--- src/main/java/com/PTM/ReadWord_copy.java
+++ src/main/java/com/PTM/ReadWord_copy.java
@@ -4,20 +4,10 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.usermodel.*;
 import org.junit.Test;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Iterator;
+import java.io.*;
 import java.util.List;
-import java.util.Objects;
 
 public class ReadWord_copy {
-
-    private String cellOne;
-    private String cellTwo;
-    private String cellThree;
-    private String cellFour;
-    public String searchString = cellOne + cellTwo + cellThree + cellFour;
 
     //Read table cell
     @Test
@@ -25,25 +15,58 @@ public class ReadWord_copy {
 
         FileInputStream fis = new FileInputStream("TC602 Vol 22.docx");
         XWPFDocument doc = new XWPFDocument(OPCPackage.open(fis));
-        int count = 0;
-        int internalcount = 0;
         List<XWPFTable> tables = doc.getTables();
         for (XWPFTable table : tables) {
-//            System.out.println(table.getText());
-            count++;
-            if (table.getRow(0).getCell(0).getText().contains("Tag")){
-//                System.out.println(table.getText());
-                internalcount++;
+            if (table.getRow(0).getCell(0).getText().contains("Tag")) {
+
+                for ( XWPFTableRow row : table.getRows() ){
+                    if (row.getCell(0).getText().contains("Tag") && row.getCell(1).getText().contains("Sub 1")
+                            && row.getCell(2).getText().contains("Sub 2") && row.getCell(3).getText().contains("Sub 3")
+                            && row.getCell(4).getText().contains("Description")) {
+                        continue;
+                    }
+                    if (!row.getCell(0).getText().startsWith("0")){
+                        continue;
+                    }
+                    String cellOne = row.getCell(0).getText();
+                    String cellTwo = row.getCell(1).getText();
+                    String cellThree = row.getCell(2).getText();
+                    String cellFour = row.getCell(3).getText();
+                    String searchString = cellOne + cellTwo + cellThree + cellFour;
+                    System.out.println("Concatinated string is: " + searchString);
+                    searchString(searchString);
+
+                }
             }
-//
-//            for (XWPFTableRow row : table.getRows()) {
-//
-//
-//            }
 
         }
-        System.out.println("Total no of tables: "  + count);
-        System.out.println("Total no of tables: "  + internalcount);
+
+    }
+
+
+    private void searchString(String searchString) throws IOException{
+
+        //Open File
+        BufferedReader bf = new BufferedReader(new FileReader("c:\\test.txt"));
+        // Start a line count and declare a string to hold our current line.
+        int linecount = 0;
+        String line;
+
+        System.out.println("Searching for " + searchString);
+
+        while (( line = bf.readLine()) != null)
+        {
+//            // Increment the count and find the index of the word
+//            linecount++;
+//            int indexfound = line.indexOf(args[0]);
+//            // If greater than -1, means we found the word
+//            if (indexfound > -1) {
+//                System.out.println("Word was found at position " + indexfound + " on line " + linecount);
+//            }
+        }
+        // Close the file after done searching
+        bf.close();
+
 
     }
 }
---
+++ src/main/java/com/PTM/application.java
@@ -0,0 +1,4 @@
+package com.PTM;
+
+public class application {
+}
